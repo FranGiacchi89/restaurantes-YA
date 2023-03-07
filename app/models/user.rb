923a5705b14eb0d98f370c1d7a3128af19457d6d
Francisco Giacchi <franciscogiacchi@outlook.com>
2023-03-07 18:24:40 -0300

    Added validations and asociations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Ascociaciones
+  has_many :restaurants # habilita el metodo .restaurants que puedo aplicarle un each
+
+  # user = User.first
+  # user.restaurants.each do |restaurant|
+
+  # Validaciones
+  validates :name, :address, presence: true
+
 end
--- db/migrate/20230307210427_create_schedules.rb
+++ db/migrate/20230307210427_create_schedules.rb
@@ -3,7 +3,7 @@ class CreateSchedules < ActiveRecord::Migration[7.0]
     create_table :schedules do |t|
       t.integer :day
       t.time :open_at
-      t.string :close_at
+      t.time :close_at
       t.references :restaurant, null: false, foreign_key: true
 
       t.timestamps
