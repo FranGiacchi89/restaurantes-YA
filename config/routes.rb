f0f3c0924e48be3332577740f103226d2f664cd6
Francisco Giacchi <franciscogiacchi@outlook.com>
2023-03-31 11:56:10 -0300

    Cambios varios

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  # devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', password: 'forgot', confirmation: 'confirm', unlock: 'unlock', registration: 'register', sign_up: 'signup' }
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
